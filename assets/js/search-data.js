e8161821acb4c96fa7b1ce1bcc2e710305b22f60
aferragu <andres.ferragut@gmail.com>
2026-04-29 23:43:00 +0000

    Deploying to gh-pages from @ aferragu/aferragu.github.io@768a21162f1223ee0992996852dd17552e2ce390 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "The following is a complete list of my publications.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
